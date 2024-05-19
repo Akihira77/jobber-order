69bff5f9687a8aba84dbe1d760fc8879b5f75778
Akihira77 <tik.dikawahyu123@gmail.com>
2024-05-19 13:38:31 +0700

    fix main branch

--- src/routes.ts
+++ src/routes.ts
@@ -2,11 +2,12 @@ import { Application } from "express";
 import { healthRoutes } from "@order/routes/health.route";
 import { orderRoutes } from "@order/routes/order.route";
 import { notificationRoutes } from "@order/routes/notification.route";
+import { verifyGatewayRequest } from "@Akihira77/jobber-shared";
 
 const BASE_PATH = "/api/v1/order";
 
 export function appRoutes(app: Application): void {
     app.use("", healthRoutes());
-    app.use(BASE_PATH, orderRoutes());
-    app.use(BASE_PATH, notificationRoutes());
+    app.use(BASE_PATH, verifyGatewayRequest, orderRoutes());
+    app.use(BASE_PATH, verifyGatewayRequest, notificationRoutes());
 }
